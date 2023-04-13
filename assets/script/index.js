f72e04a97bf1021833dd77782b30146791d86bb1
Jayvee Milana <jmmilana12@gmail.com>
2023-04-13 16:23:41 -0500

    login function added

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,3 +8,23 @@ Login Page
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 'use strict';
+
+// Login
+
+const usernameTxt = document.querySelector('.username');
+const passwordTxt = document.querySelector('.password');
+const loginBtn = document.querySelector('.login-btn');
+const incorrect = document.querySelector('.incorrect');
+
+loginBtn.addEventListener('click', () => {
+  if (usernameTxt.value === localStorage.getItem('username') &&
+      passwordTxt.value === localStorage.getItem('password')) {
+        window.location.href = `./home.html`;
+      } else {
+        incorrect.innerHTML = 'Incorrect Username/Password';
+      }
+});
+
+// Local Storage Password
+localStorage.setItem("username", "JayveeMilana12");
+localStorage.setItem("password", "12345!");
